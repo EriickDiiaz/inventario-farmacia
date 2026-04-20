2c69d97da045cbee58832ee3b77402a54619d8e6
EriickDiiaz <erick.diaz.1.2@gmail.com>
2026-04-20 11:08:50 -0400

    Se crean migraciones, modelos y controladores para los modulos Categorias y Productos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::resource('categorias', CategoriaController::class);
+    Route::resource('productos', ProductoController::class);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
